091f7752925336f691ceb7d7d2b847835174e1d3
Edgar Ruiz <edgararuiz@gmail.com>
2021-03-21 09:30:32 -0500

    Adds dbt_test_filter()

--- R/parse-tests.R
+++ R/parse-tests.R
@@ -14,3 +14,17 @@ dbt_test_mutate <- function(test_expression = fld_double + 1,
 
   all(sr == tr)
 }
+
+#' @export
+dbt_test_filter <- function(test_expression = fld_double > 2,
+                            test_field = fld_double,
+                            target_table = testdata,
+                            source_table = testdata) {
+  sm <- filter({{source_table}}, {{test_expression}})
+  sr <- pull(sm, {{test_field}})
+
+  tm <- filter({{source_table}}, {{test_expression}})
+  tr <- pull(tm, {{test_field}})
+
+  all(sr == tr)
+}
