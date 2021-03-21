789c5c0a2509821e7bccead2627520d209e07f4e
Edgar Ruiz <edgararuiz@gmail.com>
2021-03-21 09:09:17 -0500

    Adds dbt_test_mutate()

--- R/parse-tests.R
+++ R/parse-tests.R
@@ -1,3 +1,16 @@
-#' @importFrom yaml read_yaml
+#' @import yaml
+#' @import rlang
+#' @import dplyr
 #' @export
-parse_test_file <- function(file_path = "inst/tests/math-basic.yml") read_yaml(file_path)
+dbt_test_mutate <- function(test_expression = fld_double + 1,
+                            target_table = testdata,
+                            source_table = testdata) {
+
+  sm <- mutate({{source_table}}, x = {{test_expression}})
+  sr <- pull(sm, x)
+
+  tm <- mutate({{target_table}}, x = {{test_expression}})
+  tr <- pull(tm, x)
+
+  all(sr == tr)
+}
